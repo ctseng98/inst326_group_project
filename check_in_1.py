df8e3c7d986a45d442d672f165f0d745268ab3e5
Zhaojie Yin <yinzhaojie@gmail.com>
2020-11-23 04:52:53 -0500

    Implement tarots function
    implement and add functions to this method

--- check_in_1.py
+++ check_in_1.py
@@ -1,3 +1,8 @@
+import pandas as pd 
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+import webbrowser
 class Tarot:
     def __init__(self):
         """
@@ -5,6 +10,8 @@ class Tarot:
         Args:
             tarot (set): A dictionary with the name of each tarot card as values. 
         """
+        self.tarot = pd.read_csv('T.csv')
+        
     def pairing(self, number):
         """Pairs the number from Number class to a corresponding tarot card
         Args:
@@ -12,7 +19,100 @@ class Tarot:
         Returns:
             A tarot card.
         """
-
+        self.number=number
+        select_card=self.tarot.iloc[self.number].to_dict()
+    
+        # return print(f"{select_card.Name} \n--- Death Card: {select_card.Desc}\n--- Reversed Death Card: {select_card.Rdesc}")
+        return select_card 
+    def image(self,number):
+        self.number=number
+        # self.name=pairing(number).get('Name')
+        name=pairing(self.number).get('Name')
+        url=pairing(self.number).get('Url')
+        if name.startswith('The'):
+            
+            html = urlopen('{url}')
+            bs = BeautifulSoup(html, 'html.parser')
+            images = bs.find('img', {'src':re.compile('/images/tarotcards/')})
+            new_url=images['src']  
+            webbrowser.open(f'https://www.tarotcardmeanings.net{new_url}')
+        elif name.endswith('Wands'):
+            url=list()
+            html = urlopen('https://www.tarotcardmeanings.net/minorarcana/tarot-wands.htm')
+            bs = BeautifulSoup(html, 'html.parser')
+            images = bs.find_all('img', {'src':re.compile('/tarot-wands-')})
+            for image in images: 
+                url.append(image['src'])
+            first_word=name.endswith('Wands').split()[0]    
+            corrspoding_num=num(first_word)
+            webbrowser.open(url[int(corrspoding_num)-1])
+            
+        
+        elif name.endswith('Swords'):
+            url=list()
+            html = urlopen('https://www.tarotcardmeanings.net/minorarcana/tarot-wands.htm')
+            bs = BeautifulSoup(html, 'html.parser')
+            images = bs.find_all('img', {'src':re.compile('/tarot-swords-')})
+            for image in images: 
+                url.append(image['src'])
+            first_word=name.endswith('Swords').split()[0]    
+            corrspoding_num=num(first_word)
+            webbrowser.open('https://www.tarotcardmeanings.net/'+url[int(corrspoding_num)-1])
+        
+        elif name.endswith('Cups'):
+            url=list()
+            html = urlopen('https://www.tarotcardmeanings.net/minorarcana/tarot-wands.htm')
+            bs = BeautifulSoup(html, 'html.parser')
+            images = bs.find_all('img', {'src':re.compile('/tarot-cups-')})
+            for image in images: 
+                url.append(image['src'])
+            first_word=name.endswith('Cups').split()[0]    
+            corrspoding_num=num(first_word)
+            webbrowser.open('https://www.tarotcardmeanings.net/'+url[int(corrspoding_num)-1])
+        
+        elif name.endswith('Pentacles'):
+            url=list()
+            html = urlopen('https://www.tarotcardmeanings.net/minorarcana/tarot-wands.htm')
+            bs = BeautifulSoup(html, 'html.parser')
+            images = bs.find_all('img', {'src':re.compile('/tarot-pentacles-')})
+            for image in images: 
+                url.append(image['src'])
+            first_word=name.endswith('Pentacles').split()[0]    
+            corrspoding_num=num(first_word)
+            webbrowser.open('https://www.tarotcardmeanings.net/'+url[int(corrspoding_num)-1])
+            
+            
+    def num(self,label):
+        self.label=label
+        if label=='Ace':
+            return 1
+        elif label=='Two':
+            return 2
+        elif label=='Three':
+            return 3
+        elif label=='Four':
+            return 4
+        elif label=='Five':
+            return 5
+        elif label=='Six':
+            return 6
+        elif label=='Seven':
+            return 7
+        elif label=='Eight':
+            return 8
+        elif label=='Nine':
+            return 9
+        elif label=='Ten':
+            return 10
+        elif label=='Page':
+            return 11   
+        elif label=='Kight':
+            return 12      
+        elif label=='Queen':
+            return 13
+        elif label=='King':
+            return 14
+        
 class Music:
     def __init__(self):
         """Initializes a set of YouTube music by reading a csv file containing the urls.
