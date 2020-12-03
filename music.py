1e92a828c4c1a7be4d9c99b5ba300bfcb168ac1f
ctseng98 <ctseng98@terpmail.umd.edu>
2020-12-03 18:00:27 -0500

    Minor fixes for Music.py

--- music.py
+++ music.py
@@ -1,21 +1,25 @@
-import pandas as pd 
+import pandas as pd
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
 import webbrowser
 from playsound import playsound
+from argparse import ArgumentParser
+import sys
+import feedparser
+
 
 class Music:
     def __init__(self):
         """Initializes a set of YouTube music by reading a csv file containing the urls.
         Args:
-            urls (set): A set of urls that links to a YouTube music video.    
+            urls (set): A set of urls that links to a YouTube music video.
         """
         self.url = url
         self.feed = feedparser.parse(url)
-        
+
         pass
-    
+
     def get_links(self):
         """Returns a list of tuples of the title and url if have type audio
         Returns:
@@ -26,14 +30,15 @@ class Music:
             tem = [(i.title, j["href"]) for j in i.links if i.title not in output]
             output.append(tem[0])
         return output
-    
-    def play(self)
-        """ automatically plays the youtube video
+
+    def play(self):
+        """automatically plays the youtube video
         Returns:
-            plays youtube video that was generated. 
+            plays youtube video that was generated.
         """
         play_video = playsound(self.sound)
         return play_video
+
 
 def main(url):
     """ Extract titles and links from an RSS feed. """
@@ -41,9 +46,10 @@ def main(url):
     l = fw.get_links()
     for title, link in fw.get_links():
         print(f"{title} | {link}")
-        
+
+
 def parse_args(arglist):
-""" Parse command-line arguments. """
+    """ Parse command-line arguments. """
     parser = ArgumentParser()
     parser.add_argument("url", help="url of the RSS feed of a podcast")
     return parser.parse_args(arglist)
@@ -51,5 +57,4 @@ def parse_args(arglist):
 
 if __name__ == "__main__":
     args = parse_args(sys.argv[1:])
-    main(args.url) 
-
+    main(args.url)
