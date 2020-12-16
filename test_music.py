87bf3bc1ad93446eb5a3ad3c183f831e3daf7308
leighmenendez <lmenende@terpmail.umd.edu>
2020-12-16 12:53:24 -0500

    added to get_links test

--- test_music.py
+++ test_music.py
@@ -1,5 +1,9 @@
 from music import Music
+import feedparser
+
 
 def test_get_links():
-     assert url = "http://www.youtube.com/feeds/videos.xml?playlist_id=PLsH19NHPTD44m-5CCyx5jKLKAGir3iL2L"
+     url = "http://www.youtube.com/feeds/videos.xml?playlist_id=PLsH19NHPTD44m-5CCyx5jKLKAGir3iL2L"
+     feed = feedparser.parse(url) 
+     assert get_links(url) == output
     
